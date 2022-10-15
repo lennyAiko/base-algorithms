44079b44c832a4c590eff7383d8b7dc65dcac834
Lennox Charles <lennyaiko17@gmail.com>
2022-10-15 22:50:24 +0100

    initial commit

---
+++ binary.py
@@ -0,0 +1,31 @@
+# Collect user input
+
+value = int(input("Enter value you want to convert to binary: "))
+
+# set up variables and list to store binary
+
+binary_values = []
+reminder = 0
+divided_value = 0
+count = 0
+
+# perform modulos operation
+
+for i in range(value-1):
+    if count < 1:
+        divided_value = value // 2
+        reminder = value % 2
+        count+=1
+        binary_values.append(reminder)
+    reminder = divided_value % 2
+    divided_value = divided_value // 2
+    binary_values.append(reminder)
+
+print(f"The binary conversion of {value} is: ", end="")
+
+for i in binary_values[::-1]:
+    print(i, end="")
+
+
+
+
