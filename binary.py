ab2846d83d185971ccbb55e21326542c26bd501a
Lennox Charles <lennyaiko17@gmail.com>
2022-10-16 17:07:19 +0100

    made changes to output

--- binary.py
+++ binary.py
@@ -15,7 +15,7 @@ for i in range(value-1):
     if count < 1:
         divided_value = value // 2
         reminder = value % 2
-        count+=1
+        count += 1
         binary_values.append(reminder)
     reminder = divided_value % 2
     divided_value = divided_value // 2
@@ -23,7 +23,11 @@ for i in range(value-1):
 
 print(f"The binary conversion of {value} is: ", end="")
 
-for i in binary_values[::-1]:
+binary = binary_values[::-1]
+
+start = binary.index(1)
+
+for i in binary[start:]:
     print(i, end="")
 
 
